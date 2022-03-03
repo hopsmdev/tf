3550ffee6c3ca1e482ca5f330508ba78c6800333
MC <hopsm.dev@gmail.com>
2022-03-03 21:19:47 +0100

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -12,19 +12,19 @@ provider "aws" {
   region = "us-east-1"
   access_key = ""
   secret_key = ""
-
-
 }
 
-
-resource "aws_s3_account_public_access_block" "example" {
-  block_public_acls   = true
-  block_public_policy = true
-}
 
 resource "aws_s3_bucket" "mybucket" {
   bucket = "twinkiestatebucket12233444444"
 
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  block_public_acls   = true
+  block_public_policy = true
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "s3_encryption_configuration" {
