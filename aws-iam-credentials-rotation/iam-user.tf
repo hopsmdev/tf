f3c4256e7a4d12ca2fab7e1f5aa8b998c3f4bfa8
MC <hopsm.dev@gmail.com>
2023-01-23 13:32:19 +0100

    force destroy iam user

--- aws-iam-credentials-rotation/iam-user.tf
+++ aws-iam-credentials-rotation/iam-user.tf
@@ -1,6 +1,7 @@
 resource "aws_iam_user" "iam_user" {
-    count = var.create_iam_user == true ? 1 : 0
-    name = var.iam_user_name
+    count         = var.create_iam_user == true ? 1 : 0
+    name          = var.iam_user_name
+    force_destroy = true
 }
 
 resource "aws_iam_access_key" "iam_user_credentials" {
